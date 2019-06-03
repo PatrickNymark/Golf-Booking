1340b4037b5acdba09e3c2c1096dd0afcfb5b90b
Patrick Nymark <pn@ctdenmark.dk>
2019-06-03 14:09:27 +0200

    removed initial files

--- server.js
+++ server.js
@@ -4,22 +4,20 @@ const mongoose = require('mongoose');
 // Initialize
 const app = express();
 
-// Routes
-const posts = require('./routes/api/post');
-
 // Database config
 const db = require('./config/keys').mongoURI;
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, {
+    useNewUrlParser: true
+  })
   .then(res => console.log('Database connected'))
   .catch(err => console.log(err));
 
 // Body-parser middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
-
-// Router middleware
-app.use('/api/posts', posts);
 
 const port = 5000 || process.env.PORT;
 
