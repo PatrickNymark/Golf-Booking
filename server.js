de203444f82d701f39efc1a46a276d4af3980016
Patrick Nymark <pn@ctdenmark.dk>
2019-06-16 19:58:59 +0200

    cleaned out old files

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const passport = require('passport');
 const logger = require('morgan');
 const errorHandler = require('./helpers/error-handler');
 
@@ -24,20 +23,6 @@ mongoose
 // body-parser middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// passport Config
-// require('./middleware/passport')(passport);
-// app.use(passport.initialize());
-
-// routes
-const bookings = require('./routes/api/bookings/bookings');
-const staff = require('./routes/api/staff/staff');
-const courses = require('./routes/api/courses/courses');
-
-// use routes
-// app.use('/api/bookings', bookings);
-// app.use('/api/staff', staff);
-// app.use('/api/courses', courses);
 
 app.use('/api/auth', require('./controllers/auth.controller'));
 app.use('/api/course', require('./controllers/course.controller'));
